ce6595fad2bedfd60c5a66ffb526b484f77f807b
Victus <resistdj@gmail.com>
2024-04-22 14:20:48 +0300

    *.sql files added

--- db_incomes_fill.sql
+++
@@ -1,5 +0,0 @@
-use FinanceManagementDb
-
-insert into Incomes (Name, Description) values ('Salary', 'Monthly salary');
-insert into Incomes (Name, Description) values ('Deposit Interest Income', 'Income generated from interest on deposits');
-
