0b4468ad072186b377a4040ff9586af1f8388c4f
Victus <resistdj@gmail.com>
2024-09-13 13:27:32 +0300

    Operation Type dialogs added

--- Task11_Common/ViewModels/OperationTypeViewModelCommon.cs
+++ Task11_Common/ViewModels/OperationTypeViewModelCommon.cs
@@ -10,7 +10,10 @@ namespace Task11_Common.ViewModels
 
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+        
         public bool IsIncome { get; set; }
+        
+        [Required(ErrorMessage = "Description is required")]
         public string? Description { get; set; }
     }
 }
